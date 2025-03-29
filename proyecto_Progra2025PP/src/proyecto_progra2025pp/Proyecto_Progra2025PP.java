b8718e42560a9a7c948bdb1cf20abf94e34d2393
AliciaSazo <csazox@miumg.edu.gt>
2025-03-28 22:08:50 -0600

    subiendo interfaz grafica

---
+++ proyecto_Progra2025PP/src/proyecto_progra2025pp/Proyecto_Progra2025PP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_progra2025pp;
+
+/**
+ *
+ * @author ALIS
+ */
+public class Proyecto_Progra2025PP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
